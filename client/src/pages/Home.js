87a85bd562e1e48cf286b691617b400d36a70556
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-06-21 16:51:37 +0530

    lorem done

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <p>Welcome to harryblogs!</p>
+        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae tempore ducimus officiis, iusto, quam, voluptatum accusamus architecto quas ad voluptatibus omnis nostrum provident quos aut odio quibusdam soluta qui corrupti.</h1>
+    </div>
+
+  )
+}
+
+export default Home
