9383f39625b354cdb4b015b1d3c4748214eadf9c
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-06-22 13:19:17 +0530

    updated settings

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,11 +4,7 @@ const Home = () => {
   return (
     <div>
         <p>Welcome to harryblogs!</p>
-        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Corporis provident architecto doloribus omnis ipsam? 
-            Enim tempora libero sit magnam est qui, dolore nihil doloremque ea optio facilis, 
-            accusantium autem tempore consequuntur aliquid amet accusamus quibusdam quaerat debitis voluptas minus, unde ad minima. 
-            Facilis praesentium, quam totam in mollitia suscipit quos.</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic itaque sunt accusamus qui optio minima quisquam velit consequatur debitis. Facere libero dignissimos ipsam aliquam. Sunt animi fugiat impedit veritatis libero?</h1>
     </div>
 
   )
