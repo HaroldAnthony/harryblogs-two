b015b0249b615e9367d677d72dab95ddc247f712
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-06-22 13:07:26 +0530

    lorem50 done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,11 @@ const Home = () => {
   return (
     <div>
         <p>Welcome to harryblogs!</p>
-        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae tempore ducimus officiis, iusto, quam, voluptatum accusamus architecto quas ad voluptatibus omnis nostrum provident quos aut odio quibusdam soluta qui corrupti.</h1>
+        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Corporis provident architecto doloribus omnis ipsam? 
+            Enim tempora libero sit magnam est qui, dolore nihil doloremque ea optio facilis, 
+            accusantium autem tempore consequuntur aliquid amet accusamus quibusdam quaerat debitis voluptas minus, unde ad minima. 
+            Facilis praesentium, quam totam in mollitia suscipit quos.</h1>
     </div>
 
   )
