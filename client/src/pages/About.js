4bf0ae75d841ca1e64a0e2c4382567b392d8efff
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-06-25 13:05:24 +0530

    pages created

---
+++ client/src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className='mb-20'>
+      <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+        About
+      </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa laudantium eius impedit sed, odit recusandae soluta porro ad corporis quisquam ut dolorem voluptate fugiat hic explicabo, vel atque quaerat nemo sunt placeat qui, ipsum minus totam! Laboriosam incidunt in obcaecati facere aspernatur. Beatae quia rerum molestiae, ratione nobis labore numquam!
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laudantium porro necessitatibus beatae ex, harum sit impedit rem quod dignissimos sint ipsum. Saepe sint dolorum quae, earum eius cumque corporis reprehenderit modi ipsum ipsam aspernatur totam exercitationem, necessitatibus facere distinctio facilis a officiis? Ab quam debitis, explicabo quae vero error.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore quis modi officiis perspiciatis reiciendis fuga nobis ipsa quo sed voluptatem quae, adipisci laboriosam odit cum nulla ad nisi itaque neque necessitatibus tempora alias fugiat quisquam. Nostrum, ducimus repudiandae reprehenderit voluptates delectus ullam beatae. Esse nihil modi sit fugit, obcaecati molestiae unde. Soluta nesciunt ratione omnis, quasi voluptates alias, assumenda itaque recusandae ut non facere odio! Sunt, corrupti! Pariatur voluptatem necessitatibus nihil, labore sint delectus ratione vero nisi nemo. Expedita perferendis omnis libero non debitis. Mollitia assumenda eius veniam perferendis ducimus adipisci ab, numquam tempore impedit quae rerum enim facere! Impedit?
+      </p>
+    </div>
+  );
+};
+
+export default About;
