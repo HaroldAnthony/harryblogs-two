fefbc1ae08fc1bd997b4f2c81c9156017790df59
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-07-09 13:44:00 +0530

    modded server.js

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ const PORT = process.env.PORT || 8000;
 
 //just a test route
 app.get("/", (req, res) => res.send("Hello World"));
+app.post("/",(req, res) => res.send("Hello World"));
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
