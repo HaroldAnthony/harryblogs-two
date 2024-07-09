e4b2e7fcc73a8da22f18217a7028f7d59c8ac2c9
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-07-09 13:54:24 +0530

    POST req done

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+//initializing middleware
+//earlier, it was required to install body parser, but now it is a built-in function
+//function of express. it parses incoming JSON payload
+app.use(express.json({ extended: false }));
+
 //just a test route
 app.get("/", (req, res) => res.send("Hello World"));
-app.post("/",(req, res) => res.send("Hello World"));
+app.post("/",(req, res) => res.send(`Hello ${req.body.name}`));
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
