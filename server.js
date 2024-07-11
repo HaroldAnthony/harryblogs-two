3f637b55e9f7ad580dd6c1c9984a4b54c03ea5ba
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-07-11 12:46:45 +0530

    final save before DB

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ const articlesInfo = {
 //function of express. it parses incoming JSON payload
 app.use(express.json({ extended: false }));
 
-app.post('/api/articles/:name/add-comments', (req,res) => {
-    const {username, text} = req.body
-    const articleName = req.params.name
-    articlesInfo[articleName].comments.push({username, text})
-    res.status(200).send(articlesInfo[articleName]);
+app.post("/api/articles/:name/add-comments", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+  articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName]);
 });
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
