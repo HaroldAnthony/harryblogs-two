484f76d3042b32ad38dca358d448989f204d765e
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-07-11 12:42:34 +0530

    mod

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ const articlesInfo = {
 //function of express. it parses incoming JSON payload
 app.use(express.json({ extended: false }));
 
-//just a test route
-app.get("/", (req, res) => res.send("Hello World"));
-app.post("/", (req, res) => res.send(`Hello ${req.body.name}`));
-app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
+app.post('/api/articles/:name/add-comments', (req,res) => {
+    const {username, text} = req.body
+    const articleName = req.params.name
+    articlesInfo[articleName].comments.push({username, text})
+    res.status(200).send(articlesInfo[articleName]);
+});
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
